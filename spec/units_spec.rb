641e021192b74f126603b92f2964f1a0240cc221
suleman-uzair <suleman@workytical.com>
2023-05-15 14:36:19 +0500

    changed ox to plurimath and updated mrow tags position

--- spec/units_spec.rb
+++ spec/units_spec.rb
@@ -1,16 +1,17 @@
-require 'ox'
-require 'rspec'
 require 'yaml'
+require 'rspec'
+require 'plurimath'
 
 describe 'MathML in UnitsDB' do
   unitsdb = YAML.load_file('units.yaml')
 
   unitsdb.each do |unit, unit_data|
     context "when parsing MathML for #{unit}" do
+
       it 'parses successfully' do
-        mathml_values = [unit_data['mathml']].flatten.compact
-        mathml_values.each do |mathml|
-          expect { Ox.parse(mathml) }.not_to raise_error
+        unit_symbols = unit_data.dig("unit_symbols").flatten.compact
+        unit_symbols.each do |symbols_hash|
+          expect { Plurimath::Math.parse(symbols_hash.dig("mathml"), :mathml) }.not_to raise_error
         end
       end
     end
