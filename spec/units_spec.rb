2536615b1a8cf74fcd4c486de347e83f9ac8580a
Ronald Tse <ronald.tse@ribose.com>
2025-01-10 15:45:16 +0800

    feat: add normalization and validation scripts/workflows, fixes #50

--- spec/units_spec.rb
+++ spec/units_spec.rb
@@ -1,14 +1,13 @@
-require 'yaml'
-require 'rspec'
-require 'plurimath'
+require "yaml"
+require "rspec"
+require "plurimath/math"
 
-describe 'MathML in UnitsDB' do
-  unitsdb = YAML.load_file('units.yaml')
+describe "Validate MathML in UnitsDB" do
+  unitsdb = YAML.load_file(File.join(__dir__, "../units.yaml"))
 
   unitsdb.each do |unit, unit_data|
     context "when parsing MathML for #{unit}" do
-
-      it 'parses successfully' do
+      it "parses successfully" do
         unit_symbols = unit_data.dig("unit_symbols").flatten.compact
         unit_symbols.each do |symbols_hash|
           expect { Plurimath::Math.parse(symbols_hash.dig("mathml"), :mathml) }.not_to raise_error
