6ed5803fcd0fda29d423491b9834d6e5d52fd8fa
Ronald Tse <ronald.tse@ribose.com>
2025-03-22 12:47:25 +0800

    feat: use `names` instead of `quantity_name` or `unit_name` or `name`

--- spec/units_spec.rb
+++ spec/units_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "yaml"
 require "rspec"
 require "plurimath/math"
@@ -8,9 +10,9 @@ describe "Validate MathML in UnitsDB" do
   unitsdb.each do |unit, unit_data|
     context "when parsing MathML for #{unit}" do
       it "parses successfully" do
-        symbols = unit_data.dig("symbols").flatten.compact
+        symbols = unit_data["symbols"].flatten.compact
         symbols.each do |symbols_hash|
-          expect { Plurimath::Math.parse(symbols_hash.dig("mathml"), :mathml) }.not_to raise_error
+          expect { Plurimath::Math.parse(symbols_hash["mathml"], :mathml) }.not_to raise_error
         end
       end
     end
